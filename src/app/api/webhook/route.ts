fc944d27fed15e0a1c282fe32d0348800406f374
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-09-22 18:12:06 +0300

    cors

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,11 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*', // allow all origins
+	'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
 
 export async function GET(req: NextRequest) {
-  const { method, body } = req
-  return NextResponse.json({ message: 'this is get request'})
+	const { method, body } = req
+	return NextResponse.json(
+		{ message: 'this is get request', body },
+		{ headers: corsHeaders },
+	)
 }
 
 export async function POST(req: NextRequest) {
-  const { method, body } = req
-  return NextResponse.json({ message: 'this is post request' })
+	const { method, body } = req
+	return NextResponse.json(
+		{ message: 'this is post request', body },
+		{ headers: corsHeaders },
+	)
 }
